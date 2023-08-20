2760e2a8bde3dc2d0456ea5a24c8e6ab795aed97
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-08-21 00:55:43 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,30 @@
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 import { useSelector } from 'react-redux';
-import { selectVisibleContacts } from 'redux/selectors';
+import {
+  selectVisibleContacts,
+  selectContactsCount,
+  selectIsLoading,
+  selectError,
+} from 'redux/selectors';
 
 import css from './ContactList.module.css';
 
 export const ContactList = () => {
   const filteredContacts = useSelector(selectVisibleContacts);
-
+  const count = useSelector(selectContactsCount);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
   return (
     <ul className={css.list}>
-      {filteredContacts.map(contact => (
-        <ContactListItem key={contact.id} contact={contact} />
-      ))}
+      {!count && !isLoading && !error ? (
+        <p className={css.emptyMessage}>
+          The Phonebook is empty. Add your first contact.
+        </p>
+      ) : (
+        filteredContacts.map(contact => (
+          <ContactListItem key={contact.id} contact={contact} />
+        ))
+      )}
     </ul>
   );
 };
