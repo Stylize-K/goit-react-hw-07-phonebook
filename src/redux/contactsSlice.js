3a5faec169736ce1478bf946e66d8b6fa5a3288c
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-08-20 18:13:23 +0300

    Create handleRejected and handlePending function for code optimization

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -8,42 +8,36 @@ const initialState = {
   error: null,
 };
 
+const handlePending = state => {
+  state.isLoading = true;
+  state.error = null;
+};
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 //Створюємо contactsSlice
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
   //Асинхроні редюсери (extraReducers)
   extraReducers: {
-    [fetchContacts.pending]: state => {
-      state.isLoading = true;
-      state.error = null;
-    },
+    [fetchContacts.pending]: handlePending,
     [fetchContacts.fulfilled]: (state, action) => {
       state.isLoading = false;
       state.items = action.payload;
     },
-    [fetchContacts.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [addContact.pending]: (state, action) => {
-      state.isLoading = true;
-      state.error = null;
-    },
+    [fetchContacts.rejected]: handleRejected,
+    [addContact.pending]: handlePending,
     [addContact.fulfilled]: (state, action) => {
       state.isLoading = false;
       state.error = null;
       state.items = [...state.items, action.payload];
       // state.items.push(action.payload); // можна також напряму пушити масив, бо спрацює ліба Immer та виконує оновлення імутабельно
     },
-    [addContact.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [deleteContact.pending]: (state, action) => {
-      state.isLoading = true;
-      state.error = null;
-    },
+    [addContact.rejected]: handleRejected,
+    [deleteContact.pending]: handlePending,
     [deleteContact.fulfilled]: (state, action) => {
       state.isLoading = false;
       state.error = null;
@@ -53,10 +47,7 @@ export const contactsSlice = createSlice({
       );
       state.items.splice(index, 1);
     },
-    [deleteContact.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+    [deleteContact.rejected]: handleRejected,
   },
 });
 
