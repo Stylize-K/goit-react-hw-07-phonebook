e3d9a40c19104aae6b6c15e17832f6625d7e085a
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-08-20 17:48:40 +0300

    Add spinner loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { selectContactsCount } from 'redux/selectors';
 import { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/operations';
+import { ColorRing } from 'react-loader-spinner';
 
 import css from './App.module.css';
 
@@ -38,7 +39,17 @@ export const App = () => {
         <span className={css.total_count}> {count}</span>
       </p>
       <Filter />
-      {isLoading === true && <h2>Loading...</h2>}
+      {isLoading === true && (
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+          colors={['#e15b64', '#f47e60', '#f8b26a', '#4bb36a', '#80bb3d']}
+        />
+      )}
       {error && <h2>An error occurred: {error}</h2>}
       <ContactList />
       <Toaster position="top-right" />
