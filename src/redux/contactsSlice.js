464285bcada0a0a39bb40147970408232b26bbdf
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-08-20 15:12:46 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,18 +1,5 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { getContacts } from 'services/apiService';
-
-//Створюємо асинхроний thunk для отримання списку контактів з бекенду
-export const fetchContacts = createAsyncThunk(
-  'contacts/fetchAll',
-  async function (_, { rejectWithValue }) {
-    try {
-      const data = await getContacts();
-      return data;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './operations';
 
 //Початкове значення стейту у contactSlice
 const initialState = {
@@ -25,15 +12,15 @@ const initialState = {
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
-  reducers: {
-    addContact: (state, action) => {
-      state.items = [...state.items, action.payload];
-      //   state.contacts.push(action.payload) - можна також напряму пушити масив, бо спрацює ліба Immer та виконує оновлення імутабельно
-    },
-    deleteContact: (state, action) => {
-      state.items = state.items.filter(el => el.id !== action.payload);
-    },
-  },
+  // reducers: {
+  //   addContact: (state, action) => {
+  //     state.items = [...state.items, action.payload];
+  //     //   state.contacts.push(action.payload) - можна також напряму пушити масив, бо спрацює ліба Immer та виконує оновлення імутабельно
+  //   },
+  //   deleteContact: (state, action) => {
+  //     state.items = state.items.filter(el => el.id !== action.payload);
+  //   },
+  // },
   //Асинхроні редюсери (extraReducers)
   extraReducers: {
     [fetchContacts.pending]: state => {
@@ -48,8 +35,35 @@ export const contactsSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     },
+    [addContact.pending]: (state, action) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [addContact.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.error = null;
+      state.items = [...state.items, action.payload];
+      //   state.contacts.push(action.payload) - можна також напряму пушити масив, бо спрацює ліба Immer та виконує оновлення імутабельно
+    },
+    [addContact.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    [deleteContact.pending]: (state, action) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [deleteContact.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.error = null;
+      state.items = state.items.filter(el => el.id !== action.payload);
+    },
+    [deleteContact.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions; // Експортуємо actions у зовнішній код
+// export const { addContact, deleteContact } = contactsSlice.actions; // Експортуємо actions у зовнішній код
 export const contactsReducer = contactsSlice.reducer; // Експортуємо filterReducer у зовнішній код
